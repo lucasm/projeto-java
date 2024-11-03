e6f87068cd232ee5c119f55ed580c3cf68bffecc
Lucas Menezes <lucasm@lucasm.dev>
2024-11-03 15:57:40 -0300

    v1

--- Main.java
+++ Main.java
@@ -1,13 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Usuario usuario = new Usuario("João", "joao@email.com");
-        Passagem passagem = new Passagem("São Paulo", "Rio de Janeiro", 150.0, "2024-11-15");
+
+        // 1 - Simula usuário logado
+        Usuario usuario = new Usuario("Lucas", "lucas@email.com");
+
+        // 2 - Simula preencher passagem
+        Passagem passagem = new Passagem("Belém", "Belo Horizonte", 150.0, "2024-11-15");
+
+        // 3 - Simula ônibus
         Onibus onibus = new Onibus("Mercedes", 40);
 
+        // 4 - Simula compra de passagem
         CompraDePassagem compra = new CompraDePassagem(usuario, passagem, onibus);
         compra.selecionarAssento(10);
 
-        // Realiza a compra com pagamento via cartão de crédito
+        // 5 - Simula pagamento com Cartão de Crédito
         FormaDePagamento pagamentoCartao = new PagamentoCartaoCredito();
         compra.realizarCompra(pagamentoCartao);
     }
--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 
 Projeto Java Programação II
 -Lucas Menezes e Leandro Sanches
+
+Link Doc: https://docs.google.com/document/d/1O5hoGSNU_FX1YL1_sOfq8erroCZ0_ZXnfOAt6t1krm4/edit?tab=t.0
